dddd0ec0a4cf9debb7c2ab747f3280adf79e01ec
Your Name <you@example.com>
2023-11-28 19:32:44 -0500

    created activities

--- app/src/main/java/com/example/rightmeow/MainActivity.java
+++ app/src/main/java/com/example/rightmeow/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.rightmeow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickLists(View view){
+        Intent myIntent = new Intent(getBaseContext(),Lists.class);
+        startActivity(myIntent);
+    }
+
+    public void onClickSettings(View view){
+        Intent myIntent=new Intent(getBaseContext(),Settings.class);
+        startActivity(myIntent);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/rightmeow/actualList.java
@@ -0,0 +1,14 @@
+package com.example.rightmeow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actualList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actual_list);
+    }
+}
